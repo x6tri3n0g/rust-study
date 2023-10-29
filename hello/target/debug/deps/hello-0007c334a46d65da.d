9f55d135dcd2fec3173d17d8629254df6016ed14
xtring <hh940630@gmail.com>
2023-10-30 01:25:12 +0900

    feat: gcd 함수 테스트 코드 작성

---
+++ hello/target/debug/deps/hello-0007c334a46d65da.d
@@ -0,0 +1,5 @@
+/Users/xtring/Study/rust-study/hello/target/debug/deps/hello-0007c334a46d65da: src/main.rs
+
+/Users/xtring/Study/rust-study/hello/target/debug/deps/hello-0007c334a46d65da.d: src/main.rs
+
+src/main.rs:
