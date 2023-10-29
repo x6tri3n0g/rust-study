e9a8e4529f52699bc45ff3cf257b385f3171ecc2
xtring <hh940630@gmail.com>
2023-10-30 01:58:46 +0900

    feat: 입력을 받는 main문 작성

---
+++ hello/target/debug/deps/hello-17f871eabb80e049.d
@@ -0,0 +1,5 @@
+/Users/xtring/Study/rust-study/hello/target/debug/deps/hello-17f871eabb80e049: src/main.rs
+
+/Users/xtring/Study/rust-study/hello/target/debug/deps/hello-17f871eabb80e049.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/xtring/Study/rust-study/hello/target/debug/hello: /Users/xtring/Study/rust-study/hello/src/main.rs
