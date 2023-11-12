4efc66373ff94667c043463cbddae3b5a530b75c
xtring <hh940630@gmail.com>
2023-11-13 02:29:41 +0900

    change: clean project

--- rust-kr/03_common_programming_concepts/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/xtring/Study/rust-study/rust-kr/03_common_programming_concepts/variables/target/debug/variables: /Users/xtring/Study/rust-study/rust-kr/03_common_programming_concepts/variables/src/main.rs
